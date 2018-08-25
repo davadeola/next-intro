58c48748b04a047e89ee791ad1bfbafdbc4530a2
David Adeola <adeoladavid15@gmail.com>
2018-08-25 19:25:43 +0300

    Fixed the error by moving the withRouter function to the content component

--- pages/post.js
+++ pages/post.js
@@ -2,16 +2,16 @@
 import {withRouter} from 'next/router';
 import Layout from '../components/Layout.js';
 
-const Content=(props)=>{
+const Content= withRouter((props)=>(
   <div>
     <h1>{props.router.query.title}</h1>
     <p>This the blog's content</p>
   </div>
-}
-const Page=withRouter((props)=>(
+))
+const Page=(props)=>(
   <Layout>
     <Content/>
   </Layout>
-))
+)
 
 export default Page;

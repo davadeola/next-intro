0a08751c8e6883e6ad4fc6350c10f04702df933f
David Adeola <adeoladavid15@gmail.com>
2018-08-25 14:23:52 +0300

    Added the post links to inex.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
-
 import Layout from '../components/Layout.js';
+import Link from 'next/link';
 
-const Index=()=>{
-  return(
-    <Layout>
-      <p>Hello Next.js</p>
-    </Layout>
-  )
+const Index = () => {
+
+  const PostLink = (props) => (<li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>)
+
+  return (<Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js"/>
+      <PostLink title="Learn Next.js is awesome"/>
+      <PostLink title="Deploy apps with Zeit"/>
+    </ul>
+  </Layout>)
 }
 
 export default Index;
